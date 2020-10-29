18291a52bf203bff99604ebf875373a752787846
RBorshcheva <raisaborshcheva@mail.ru>
2020-10-29 10:21:59 +0300

    new questions

--- js/tikTakBoomTasks.js
+++ js/tikTakBoomTasks.js
@@ -28,14 +28,77 @@ const tasks = `
             "answer2": { "result": true, "value": "Рим" },
             "answer3": { "result": false, "value": "Милан" },
             "answer4": { "result": false, "value": "Палермо" }
-        }
+        },
         {
             "question": "12^2 = ",
             "answer1": { "result": true, "value": "144" },
             "answer2": { "result": false, "value": "169" },
             "answer3": { "result": false, "value": "121" },
-            "answer4": { "result": false, "value": "122" },
-            "answer5": { "result": false, "value": "24" }
+            "answer4": { "result": false, "value": "24" }
         },
+        {
+            "question": "Столица Испании?",            
+            "answer1": { "result": true, "value": "Мадрид" },
+            "answer2": { "result": false, "value": "Севилья" },
+            "answer3": { "result": false, "value": "Барселона" },
+            "answer4": { "result": false, "value": "Валенсия" }
+        },
+        {
+            "question": "Сколько материков на Земле?",
+            "answer1": { "result": true, "value": "6" },
+            "answer2": { "result": false, "value": "5" },
+            "answer3": { "result": false, "value": "7" },
+            "answer4": { "result": false, "value": "4" }
+        },
+        {
+            "question": "Какой цикл в JavaScript проверяет условие перед каждой итерацией?",
+            "answer1": { "result": true, "value": "while" },
+            "answer2": { "result": false, "value": "while...until" },
+            "answer3": { "result": false, "value": "for" },
+            "answer4": { "result": false, "value": "do...while" }
+        },
+        {
+            "question": "Какой цикл в JavaScript проверяет условие после каждой итерации?",
+            "answer1": { "result": true, "value": "do...while" },
+            "answer2": { "result": false, "value": "while...until" },
+            "answer3": { "result": false, "value": "for" },
+            "answer4": { "result": false, "value": "while" }
+        },
+        {
+            "question": "Какой цикл в JavaScript проверяет условие перед каждой итерацией, можно задать дополнительные настройки?",
+            "answer1": { "result": true, "value": "for" },
+            "answer2": { "result": false, "value": "while" },
+            "answer3": { "result": false, "value": "for...in" },
+            "answer4": { "result": false, "value": "do...while" }
+        },
+        {
+            "question": "Какова пространственная сложность сортировки слиянием?",
+            "answer1": { "result": true, "value": "O(n)" },
+            "answer2": { "result": false, "value": "O(n*log(n))" },
+            "answer3": { "result": false, "value": "O(n^2)" },
+            "answer4": { "result": false, "value": "O(1)" }
+        },
+        {
+            "question": "Какие бывают встроенные циклические конструкции в JavaScript?",
+            "answer1": { "result": true, "value": "for...in" },
+            "answer2": { "result": false, "value": "forEach" },
+            "answer3": { "result": false, "value": "do...loop" },
+            "answer4": { "result": false, "value": "for...next" }
+        },
+        {
+            "question": "Какая из представленных стран НЕ входит в состав ООН?",
+            "answer1": { "result": false, "value": "Германия" },
+            "answer2": { "result": false, "value": "Бельгия" },
+            "answer3": { "result": false, "value": "Россия" },
+            "answer4": { "result": true, "value": "Мальта" }
+        },
+        {
+            "question": "Какая из представленных стран НЕ входит в состав ОПЕК?",
+            "answer1": { "result": false, "value": "Иран" },
+            "answer2": { "result": false, "value": "Ирак" },
+            "answer3": { "result": false, "value": "ОАЭ" },
+            "answer4": { "result": true, "value": "Россия" }
+        },
+        
     ]
 `;
