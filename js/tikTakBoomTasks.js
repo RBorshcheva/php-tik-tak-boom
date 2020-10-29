3e4319784cdd00815b3fd306b1d03179e9712b8b
RBorshcheva <raisaborshcheva@mail.ru>
2020-10-29 10:08:31 +0300

    rules antions

--- js/tikTakBoomTasks.js
+++ js/tikTakBoomTasks.js
@@ -4,22 +4,38 @@ const tasks = `
         {
             "question": "2 * 2 = ",
             "answer1": { "result": true, "value": "4" },
-            "answer2": { "result": false, "value": "3" }
+            "answer2": { "result": false, "value": "3" },
+            "answer3": { "result": false, "value": "2" },
+            "answer4": { "result": false, "value": "1" }
         },
         {
             "question": "Как зовут крокодила, лучшего друга Чебурашки?",
             "answer1": { "result": false, "value": "Данди" },
-            "answer2": { "result": true, "value": "Гена" }
+            "answer2": { "result": true, "value": "Гена" },
+            "answer3": { "result": false, "value": "Крок" },
+            "answer4": { "result": false, "value": "Гриша" }
         },
         {
             "question": "5 + 5",
             "answer1": { "result": false, "value": "1" },
-            "answer2": { "result": true, "value": "10" }
+            "answer2": { "result": true, "value": "10" },
+            "answer3": { "result": false, "value": "5" },
+            "answer4": { "result": false, "value": "25" }
         },
         {
             "question": "Столица Италии?",
             "answer1": { "result": false, "value": "Ватикан" },
-            "answer2": { "result": true, "value": "Рим" }
+            "answer2": { "result": true, "value": "Рим" },
+            "answer3": { "result": false, "value": "Милан" },
+            "answer4": { "result": false, "value": "Палермо" }
         }
+        {
+            "question": "12^2 = ",
+            "answer1": { "result": true, "value": "144" },
+            "answer2": { "result": false, "value": "169" },
+            "answer3": { "result": false, "value": "121" },
+            "answer4": { "result": false, "value": "122" },
+            "answer5": { "result": false, "value": "24" }
+        },
     ]
 `;
